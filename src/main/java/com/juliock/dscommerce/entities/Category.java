39a6dd52488dc33a31ad16240d3425aaa0b08dc3
julioCK <bocumaa@gmail.com>
2025-04-15 10:04:38 -0300

    Product and Category

---
+++ src/main/java/com/juliock/dscommerce/entities/Category.java
@@ -0,0 +1,48 @@
+package com.juliock.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    /*  Várias instancias da entidade Category estarão relacionadas a várias instancias da entidade Product.
+    *           A relação dessa entidade com Product está mapeada em nas entidades do tipo Product pelo atributo "categories"   */
+
+    /*  Não pode haver dois produtos iguais (repetidos) listados em uma mesma categoria, portanto a interface Set é ideal aqui, visto que não admite repetições de produtos.    */
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/juliock/dscommerce/entities/Product.java
@@ -0,0 +1,91 @@
+package com.juliock.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    /*  @ManyToMany sinaliza que *varias* instancias dessa entidade estão relacionadas a varias instancias de outra entidade.
+            Esse tipo de relacionamento gera uma tabela separada de referencia contendo a chave primaria de cada uma das duas entidades */
+
+    /*  @JoinTable sinaliza para o JPA que a tabela de relacionamento (JoinTable) será criada com o nome especificado
+    *       e vai conter uma coluna (JoinColumns) para a FK da entidade Product (coluna "product_id")
+    *       e um coluna (inverseJoinColumns) para a FK da entidade Category (coluna "category_id").   */
+
+    /*  Não pode haver um produto em duas categorias iguais (repetidas), portanto a interface Set é a ideal para agrupar categorias pois nao admite repetições. */
+
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String description, Double price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Set<Category> getCategories() {
+        return new HashSet<>(this.categories);
+    }
+}
